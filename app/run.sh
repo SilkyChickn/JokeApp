0956aa435973f47f003639957c5df14490447bac
Suirad <darius-suirad@gmx.de>
2019-11-19 15:32:48 +0100

    Prepare for start.

--- app/run.sh
+++ app/run.sh
@@ -2,12 +2,13 @@
 
 # Install npm packages
 echo "Installing required npm packages"
-npm install
+npm install --silent
+npm install react-scripts@3.0.1 -g --silent
 echo "npm packages installed"
 
 # Run the react application
 echo "Starting app"
-npm run start
+npm start
 
 # Keep container running
 tail -f /dev/null
